71f2630238c52430be5f0e0755c797fc5d7a0bbf
Jacques Nadeau <jacques@apache.org>
2024-12-20 09:47:37 -1000

    try lib change

--- cgo_static.go
+++ cgo_static.go
@@ -4,7 +4,7 @@ package duckdb_go_extensions
 
 ////-lsubstrait_extension
 /*
-#cgo LDFLAGS: -licu_extension -ltpch_extension -ltpcds_extension -lsubstrait_extension
+#cgo LDFLAGS: -licu_extension -ltpch_extension -ltpcds_extension -lsubstrait_extension -lparquet_extension -lduckdb_mbedtls
 #cgo darwin,amd64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_amd64
 #cgo darwin,arm64 LDFLAGS: -lc++ -L${SRCDIR}/deps/darwin_arm64
 #cgo linux,amd64 LDFLAGS: -lstdc++ -lm -ldl -L${SRCDIR}/deps/linux_amd64
