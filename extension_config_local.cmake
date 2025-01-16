82e12136cdf2e8fc2de378d6ee2e298845a3c883
David Sisson <EpsilonPrime@users.noreply.github.com>
2025-01-15 21:35:04 -0800

    Point to iceberg support

--- extension_config_local.cmake
+++ extension_config_local.cmake
@@ -18,8 +18,8 @@ duckdb_extension_load(httpfs
 duckdb_extension_load(tpch)
 duckdb_extension_load(tpcds)
 duckdb_extension_load(substrait
-    GIT_URL https://github.com/rymurr/duckdb-substrait-extension
-    GIT_TAG 23ffe479c9d4500f42decf4c50cc5fcf15588bd7
+    GIT_URL https://github.com/substrait-io/duckdb-substrait-extension
+    GIT_TAG 75ac47c58e65b14832a29dbd06d163c258e61955
 )
 duckdb_extension_load(iceberg
     GIT_URL https://github.com/duckdb/duckdb-iceberg
